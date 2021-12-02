b0fad0de84644212645232159ff204774cc246af
Amogh <amoghmanjunath99@gmail.com>
2021-12-02 17:06:23 +0530

    added margin

--- app/views/about.py
+++ app/views/about.py
@@ -2,12 +2,6 @@ from flask import render_template, request, redirect
 from app import app, db
 from app.models.profile import Profile 
 
-# about page2
-# line 2
-# line 2a
-# line 3
-# about page
-# about4
 
 @app.route('/about', methods=['GET'])
 def about():
