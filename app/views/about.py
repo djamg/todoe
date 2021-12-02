6cfbe53df5b1f73b89ff61cb8e37690b4d159d5e
Amogh <amoghmanjunath99@gmail.com>
2021-12-02 16:20:23 +0530

    added comments

--- app/views/about.py
+++ app/views/about.py
@@ -2,7 +2,7 @@ from flask import render_template, request, redirect
 from app import app, db
 from app.models.profile import Profile 
 
-# about page
+# about page2
 @app.route('/about', methods=['GET'])
 def about():
     return render_template('about.html')
