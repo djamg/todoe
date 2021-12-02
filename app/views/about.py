5a2c1edd04e07925ad162c17d552b404a881675c
Amogh <amoghmanjunath99@gmail.com>
2021-12-02 16:21:33 +0530

    added comments

--- app/views/about.py
+++ app/views/about.py
@@ -3,6 +3,9 @@ from app import app, db
 from app.models.profile import Profile 
 
 # about page2
+# line 2
+# line 2a
+# line 3
 @app.route('/about', methods=['GET'])
 def about():
     return render_template('about.html')
