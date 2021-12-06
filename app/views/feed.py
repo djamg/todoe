8856df41508034caed6884676a20582732dc689d
Amogh <amoghmanjunath99@gmail.com>
2021-12-06 19:40:33 +0530

    Added login

--- app/views/feed.py
+++ app/views/feed.py
@@ -1,8 +1,10 @@
 from flask import render_template, request, redirect
 from app import app, db
-from app.models.profile import Profile 
+from app.models.profile import Profile
+from flask_login import login_required
 
 
 @app.route('/feed', methods=['GET'])
+@login_required
 def feed():
     return render_template('feed.html')
